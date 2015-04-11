89871f5bfdc3c99153f42dc9e15c125ad8df26a2
Rafal Korkosz <korkosz.rafal@gmail.com>
2015-04-11 22:45:42 +0200

    add sitemaps, robots

--- korkoszeu/urls.py
+++ korkoszeu/urls.py
@@ -2,8 +2,21 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.flatpages.sitemaps import FlatPageSitemap
+from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
+
+sitemaps = {
+    'flatpages': FlatPageSitemap
+}
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^robots\.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
     url(r'', include('django.contrib.flatpages.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns.append(url(r'^__debug__/', include(debug_toolbar.urls)))
