09213362bace601886a6ac1e40dc906af832867d
Rafał Korkosz <rkorkosz@milosolutions.com>
2015-04-13 12:05:05 +0200

    test settings memcached, redis

--- korkoszeu/settings/test.py
+++ korkoszeu/settings/test.py
@@ -9,3 +9,16 @@ DATABASES = {
         'HOST': '127.0.0.1',
     }
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_bmemcached.memcached.BMemcached',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+SESSION_REDIS_HOST = 'localhost'
+SESSION_REDIS_PORT = 6379
+SESSION_REDIS_DB = 0
+SESSION_REDIS_PASSWORD = 'password'
+SESSION_REDIS_PREFIX = 'session'
